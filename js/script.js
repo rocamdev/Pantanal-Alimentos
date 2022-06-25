a0be21626a27c3fe8852f683965431bdfd591484
rodolforcampos <rodolforibeirocampos@gmail.com>
2022-06-24 23:23:17 -0300

    IMPLEMENTAR SLIDES NO SITE
    Foi finalizado o slide do quote.
    Foi criado uma base para animações que virão na page.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-new SimpleSlide({
-  slide: 'quote', // nome do atributo data-slide="principal"
-  nav: false, // se deve ou não mostrar a navegação
-  auto: true, // se o slide deve passar automaticamente
-  time: 5000, // tempo de transição dos slides
-  pauseOnHover: true, // pausa a transição automática
-});
+if(window.SimpleSlide){
+  new SimpleSlide({
+    slide: 'quote', // nome do atributo data-slide="principal"
+    nav: false, // se deve ou não mostrar a navegação
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+    pauseOnHover: true, // pausa a transição automática
+  });
+}
