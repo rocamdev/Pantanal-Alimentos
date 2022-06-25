4a28aa73022d1ff39245e0b4b04420b435d4ca9f
rodolforcampos <rodolforibeirocampos@gmail.com>
2022-06-24 22:57:47 -0300

    ADICIONAR O PLUGIN SIMPLE-SLIDE
    Este plugin será utilizado para implementar slides em alguns pontos.
    O local no momento é o quote da page parceiros.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+new SimpleSlide({
+  slide: 'quote', // nome do atributo data-slide="principal"
+  nav: false, // se deve ou não mostrar a navegação
+  auto: true, // se o slide deve passar automaticamente
+  time: 5000, // tempo de transição dos slides
+  pauseOnHover: true, // pausa a transição automática
+});
