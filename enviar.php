f967b7137d4c36ae8545fc2720279a959bf4e90f
rodolforcampos <rodolforibeirocampos@gmail.com>
2022-06-27 00:25:28 -0300

    IMPLEMENTAR BASE DE IMPLEMENTAÇÃO DO FORMULÁRIO PHP
    O formulário precisa de e-mail e senha para funcionar.
    O formulário está com falha por falta de e-mail.
    O e-mail não foi colocado pra manter a privacidade no repositorio publico.

--- enviar.php
+++ enviar.php
@@ -8,7 +8,7 @@ require "./PHPMailer/src/PHPMailer.php";
 require "./PHPMailer/src/SMTP.php";
   
 // Mudar Aqui o e-mail
-$email_envio = ""; // E-mail do site (ex: contato@seusite.com)
+$email_envio = ""; // e-mail não cadastrado pra privacidade no github
 $email_pass = ""; // Senha do e-mail
 
 $site_name = "Bikcraft"; // Nome do Site
